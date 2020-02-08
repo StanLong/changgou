073b0809742de810ef4a43041537901c3a13f98a
StanLong <youlinbhz@sina.cn>
2020-02-08 14:49:03 +0800

    相册增删改查

---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/dao/AlbumMapper.java
@@ -0,0 +1,11 @@
+package com.changgou.goods.dao;
+
+import com.changgou.goods.goods.pojo.Album;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author 矢量
+ * @date 2020/2/7-18:28
+ */
+public interface AlbumMapper extends Mapper<Album> {
+}
