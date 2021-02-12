6fca5b7727b453fbaea3f6d71c4812f4f28d9e3f
StanLong <youlinbhz@sina.cn>
2021-02-12 15:53:25 +0800

    畅购商城

--- changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/dao/CategoryBrandMapper.java
+++
@@ -1,7 +0,0 @@
-package com.changgou.goods.dao;
-
-import com.changgou.goods.pojo.CategoryBrand;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface CategoryBrandMapper extends Mapper<CategoryBrand> {
-}
--- changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/dao/SkuMapper.java
+++
@@ -1,8 +0,0 @@
-package com.changgou.goods.dao;
-
-import com.changgou.goods.pojo.Sku;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface SkuMapper extends Mapper<Sku> {
-
-}
